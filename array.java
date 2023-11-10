1e34c10c5d9b99b9c70801ee7a2b033fc0d3e48d
venkateshpandey22 <149189403+venkateshpandey22@users.noreply.github.com>
2023-11-10 18:07:58 +0530

    Add files via upload
    This is basic Array Program

---
+++ array.java
@@ -0,0 +1,33 @@
+package com.java;
+
+public class array {
+
+	public static void main(String[] args) {
+		
+	/*	int[] arr =new int[4];
+		arr[0]=25;
+        arr[1]=20;
+        arr[2]=15;
+        arr[3]=10;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+         for(int i=0;i<4;i++)
+        System.out.println(arr[i]); */
+        	
+		
+		int arr[]={10,15,20,25};
+	/*	arr[0]=25;
+        arr[1]=20;
+        arr[2]=15;
+        arr[3]=10;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+       */
+        for(int i=0;i<4;i++)
+        	System.out.println(arr[i]);
+	}
+}
