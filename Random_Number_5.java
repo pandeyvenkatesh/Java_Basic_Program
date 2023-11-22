758387505ab695edc6eaa1feb5894369ab3ae7f1
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-22 23:24:42 +0530

    Add files via upload

---
+++ Random_Number_5.java
@@ -0,0 +1,15 @@
+package com.java;
+
+public class Random_Number_5 {
+
+	public static void main(String[] args) {
+		
+   System.out.println("1th Random Number:- "+Math.random());	
+   System.out.println("2ed Random Number:- "+Math.random());
+   System.out.println("3ed Random Number:- "+Math.random());
+   System.out.println("4th Random Number:- "+Math.random());
+   System.out.println("5th Random Number:- "+Math.random());
+
+	}
+
+}
