055454610ec2a73fac8943abb3521b967129f0bd
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-23 08:46:30 +0530

    Add files via upload

---
+++ EvenOdd_6.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+		System.out.print("Enter a number You want to Check :- ");
+		int x=scn.nextInt();
+		if(x%2==0){
+			System.out.println("The Number is Even.");
+	}else
+	{
+		System.out.println("The Number is Odd.");
+}
+}
+}
