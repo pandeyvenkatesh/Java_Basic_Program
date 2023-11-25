64fd1e217f56591068e863b646045821f63e7cbc
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 11:22:46 +0530

    Add files via upload

---
+++ if_else_14.java
@@ -0,0 +1,24 @@
+package com.venkatesh;
+
+public class if_else_14 {
+
+	public static void main(String[] args) {
+		
+		String city ="Delhi";
+		
+		if(city=="Meerut") {
+			System.out.println("The City is Meerut.");
+			
+		}else if(city== "Noida") {
+			System.out.println("The city is Noida.");
+			
+		}else if(city=="Mumbai") {
+			System.out.println("The city is Mumbai.");
+		
+		}else if(city=="Delhi") {
+			System.out.println("The city is Delhi.");
+		}
+
+	}
+
+}
