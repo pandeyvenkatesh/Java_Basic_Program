34c5bbecc8bf2aadd0907b9b9c67d0b429a5a22a
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 12:15:01 +0530

    Add files via upload

---
+++ while_loop_19.java
@@ -0,0 +1,18 @@
+package com.venkatesh;
+
+public class while_loop_19 {
+
+	public static void main(String[] args) {
+		int i=0;
+		
+		System.out.print("Printing the list of first 10 even number\n");
+		while(i<=10) {
+			
+			System.out.println(i);
+	
+	    i=i+2;		
+		}
+
+	}
+
+}
