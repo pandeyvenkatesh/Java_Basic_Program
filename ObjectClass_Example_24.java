91ea34cf4ee934cbf470d7ccab0fdf8fc6d90ca5
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 14:22:00 +0530

    Add files via upload

---
+++ ObjectClass_Example_24.java
@@ -0,0 +1,21 @@
+package com.venkatesh;
+
+class Student{
+	int id;
+	String name;	
+}
+
+
+public class ObjectClass_Example_24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Student s1=new Student();
+      
+      s1.id=22;
+      s1.name="Venkatesh Pandey";
+      
+      System.out.println(s1.id+" "+s1.name);
+	}
+
+}
