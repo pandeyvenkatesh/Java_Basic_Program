c359d161b40b306703b1d768cfb24bff16c0b197
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 14:16:28 +0530

    Add files via upload

---
+++ ObjectClass_Example_23.java
@@ -0,0 +1,22 @@
+package com.venkatesh;
+
+//Java Program to illustrate how to define a class and fields  
+//Defining a Student class.  
+class Student{
+	int id;
+	String name;
+}
+
+public class ObjectClass_Example_23 {
+
+	public static void main(String[] args) {
+	
+	Student s1=new Student(); //Creating an Object on Student
+	
+	System.out.println(s1.id);
+	System.out.println(s1.name);
+		
+
+	}
+
+}
