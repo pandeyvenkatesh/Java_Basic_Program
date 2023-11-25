b0b6cb94609691fe3504526fd67d1394ae60ff13
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 13:48:50 +0530

    Add files via upload

---
+++ doWhile_loop_20.java
@@ -0,0 +1,18 @@
+package com.venkatesh;
+
+public class doWhile_loop_20 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i=0;
+		System.out.println("Printing the list of first 10 even number \n");
+		
+		do {
+			System.out.println(i);
+			i=i+2;
+		}
+		while(i<=10);
+	}
+
+}
