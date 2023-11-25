d6e45b76c4e9095c356135aa5a0d7284274c6f91
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 11:53:52 +0530

    Add files via upload

---
+++ for_loop_17.java
@@ -0,0 +1,16 @@
+package com.venkatesh;
+
+public class for_loop_17 {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		for(int i=1;i<=10;i++) {
+			
+			sum=sum+i;
+		}
+		System.out.println("The Sum of first 10 natural number is "+sum);
+
+	}
+
+}
