ca142dbb3a9778e6a5b5a640ea78e546274d243e
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 22:09:54 +0530

    Add files via upload
    Java Program to create and call a default constructor

---
+++ ObjectClass_Example_30.java
@@ -0,0 +1,22 @@
+package com.java;
+
+//Java Program to create and call a default constructor 
+
+class Bike1{
+	//creating a default constructor 
+	Bike1 (){
+		System.out.println("Bike is created");
+	}
+}
+
+//main method
+
+public class ObjectClass_Example_30 {
+
+	public static void main(String[] args) {
+		//calling a default constructor 
+		Bike1 b=new Bike1();
+
+	}
+
+}
