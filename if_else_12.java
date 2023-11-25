fbb4feb671844e397ac85991d2a63e7be96ecb53
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 11:13:17 +0530

    Add files via upload

---
+++ if_else_12.java
@@ -0,0 +1,16 @@
+package com.venkatesh;
+
+public class if_else_12 {
+
+	public static void main(String[] args) {
+		
+		int x=10;
+		int y=20;
+		
+		if(x+y>25) {
+			System.out.println("X + Y is greater than 25.");
+		}
+		
+	}
+
+}
