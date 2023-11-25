011500731f4a76cfa0a944441e42ff819f046bf8
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 13:51:36 +0530

    Add files via upload

---
+++ break_example_21.java
@@ -0,0 +1,19 @@
+package com.venkatesh;
+
+public class break_example_21 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=0;i<=10;i++) {
+			System.out.println(i);
+		
+			if(i==6) {
+				
+				break;
+			}
+			
+		}
+	}
+
+}
