f6e64c2f73e69cdb2162bfa2818645488031094f
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 13:59:13 +0530

    Add files via upload

---
+++ ContinueExample_22.java
@@ -0,0 +1,22 @@
+package com.venkatesh;
+
+public class ContinueExample_22 {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<=2;i++) {
+			
+			for(int j=i;j<=5;j++) {
+				
+				if(j==4) {
+					
+					continue;
+				}
+				
+				System.out.println(j);
+			}
+		}
+
+	}
+
+}
