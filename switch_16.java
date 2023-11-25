7cf875c5c00c99ca60d93660062b3dc5afb3b295
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-25 11:48:00 +0530

    Add files via upload

---
+++ switch_16.java
@@ -0,0 +1,28 @@
+package com.venkatesh;
+
+public class switch_16 {
+
+	public static void main(String[] args) {
+		int num=2;
+		
+		switch(num) {
+		
+		case 0: 
+			System.out.println("number is 0.");
+			break;
+			
+		case 1:
+			System.out.println("number is 1.");
+			break;
+	
+		case 2:
+			System.out.println("number is 2.");
+		    break;
+	  
+		default:
+			System.out.println(num);
+		}
+
+	}
+
+}
