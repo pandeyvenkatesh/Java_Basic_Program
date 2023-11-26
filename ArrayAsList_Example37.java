31cf014769192553f91804be6172e609a0c88e25
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-26 18:49:33 +0530

    Add files via upload

---
+++ ArrayAsList_Example37.java
@@ -0,0 +1,18 @@
+package com.Venkatesh;
+import java.util.Arrays;
+
+public class ArrayAsList_Example37 {
+
+	public static void main(String[] args) {
+		
+		//get the Arrays
+		
+		int arr[]= {23,35,36,27,37,2,47,85,3};
+		
+		// To convert the elements as the List 
+		System.out.println("Integer Array  as List "+Arrays.asList(arr));
+
+		
+	}
+
+}
