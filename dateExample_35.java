3b076cdc065fc36bc60c2ab3a96ec9e414815478
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-26 18:21:38 +0530

    Add files via upload
    In this Program we are  Print current date and time in this Program.

---
+++ dateExample_35.java
@@ -0,0 +1,17 @@
+package com.Venkatesh;
+import java.util.*;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class dateExample_35 {
+
+	public static void main(String[] args) {
+     		
+	LocalTime obj2 = LocalTime.now();
+	LocalDate obj3= LocalDate.now();
+	
+	System.out.println(obj2);
+	System.out.println(obj3);
+
+	}
+}
