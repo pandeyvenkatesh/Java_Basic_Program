c77c430adfcecf017a2d3e9fe22cccfdec2873bf
Venkatesh Pandey <149189403+pandeyvenkatesh@users.noreply.github.com>
2023-11-28 22:22:38 +0530

    Add files via upload

---
+++ Pattern_43.java
@@ -0,0 +1,28 @@
+package com.Venkatesh;
+
+/* WAP to Print Below pattern
+
+1 2 3 4 5 
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+1 2 3 4 5
+
+*/
+
+public class Pattern_43 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=5;i++) {
+			
+			for(int j=1;j<=5;j++) {
+				
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
